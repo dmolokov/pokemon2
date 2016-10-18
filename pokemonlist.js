148319152cbda911934027937f6d51e18a8a4011
dmolokov <dmitriymolokov@yahoo.com>
2016-10-18 11:32:49 +0300

    Add files via upload

--- pokemonlist.js
+++ pokemonlist.js
@@ -2,11 +2,6 @@
 const Pokemon = require('./pokemon');
 module.exports = class PokemonList extends Array {
 	
-	constructor(...pokemons_array) {
-		super();
-		this.push(...pokemons_array);
-	}
-	
 	show() {
 		console.log('Количество покемонов: ' + this.length);
 		this.forEach((item) => item.show());
@@ -14,18 +9,6 @@ module.exports = class PokemonList extends Array {
 	
 	add(name, level) {
 		this.push(new Pokemon(name, level));
-	}
-	
-	valueOf() {
-		return (this.map((item) => item.level));
-	}
-	
-	max() {
-		let max_level = Math.max.apply(null, this.valueOf());
-		return (
-		this.find((pokemon) => {
-			return pokemon.level == max_level;
-		})); 
 	}
 };
 
